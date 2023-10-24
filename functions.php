c633f150d9abacf0541f550e1ab80a0422c9bcda
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2023-10-24 18:03:39 +0330

    wp-query products , Swiper

--- functions.php
+++ functions.php
@@ -140,13 +140,16 @@ add_action( 'widgets_init', 'karenteb_widgets_init' );
 function karenteb_scripts() {
 	wp_enqueue_style( 'karenteb-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'karenteb-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'swiper-style', get_template_directory_uri().'/swiper/swiper-bundle.min.css', array(), _S_VERSION );
+
 
 	wp_enqueue_script('jquery');
 	wp_enqueue_style( 'additional-style', get_template_directory_uri().'/additional.css', array(), _S_VERSION );
 	wp_enqueue_script( 'karenteb-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'karenteb-home', get_template_directory_uri() . '/js/home.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'karenteb-headercontrol', get_template_directory_uri() . '/js/headercontrol.js', array(), _S_VERSION, true );
-
+	wp_enqueue_script( 'swiper', get_template_directory_uri() . '/swiper/swiper-bundle.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'karenteb-swiperSlides', get_template_directory_uri() . '/js/swiperSlides.js', array(), _S_VERSION, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
