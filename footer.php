50a26a8635d0fbc74cf0bd1c28b83aba44c36e2b
starmahmoodi <starmahmoodi@gmail.com>
2024-01-27 17:42:06 +0330

    11

--- footer.php
+++ footer.php
@@ -13,26 +13,26 @@
 		<div class="mainFooterBack">
 			<div class="mainFooter mainView">
 				<div class="footerColumn1 footerColumns">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 466.8 236.4"><path fill="#fff" d="M230.4 0h71.7v40.9h12.1l54-40.9h90.7l-115 101.7 122.9 134.7h-90.6L301.3 160v76.4h-70.9V0z"/><path fill="#0b3152" d="M362.9 122.6h-40.7l-7.9-19.8-28.4 52.1-20.5-53.6-11.2 20.5h-23.8v114.6h70.9V160l74.9 76.4h90.6L362.9 122.6z"/><path fill="#187dba" d="M343.9 101.7 458.9 0h-90.7l-54 40.9h-12.1V0h-71.7v110.8h16.9L267 76.1l20.5 52.8 27.6-52.1 15 34.7h22.8l-9-9.8z"/><path d="M99.3 59.8h16.8v27.7c0 10.7-2.7 19.1-8 25.2s-14.8 10.2-26.3 10.2h-5.1V106h5.1c6.3 0 10.7-1.4 13.4-4.2s4.1-7.6 4.1-14.3Zm100.5 9a23 23 0 0 1 1.9 8.5v.4a15.1 15.1 0 0 1-1.3 6.1 17.3 17.3 0 0 1-3.2 4.9 20 20 0 0 1-6.9 4.4 39 39 0 0 1-14.1 2.2h-29.1a26.3 26.3 0 0 1-10.3-2 17.8 17.8 0 0 1-6-4.1 19.1 19.1 0 0 1-4-6 23.8 23.8 0 0 1-2-10.2V21.2h16.8V73c0 2.1.4 3.5 1.1 4.3s2.2 1.1 4.4 1.1h29.1c5.4 0 8.5-.2 9.6-.8l-.6-1.4-1.3-2.2a57.2 57.2 0 0 0-6-7.3c-6-6.4-11.1-11.6-15.5-15.9l-2.2-2-1.5-1.4c-.1-.2-.3-.3-.5-.5l-.3-.3h-.4l2-14.1 45-20.5 7 15.4-33.7 15c2.9 2.9 7.1 7.2 12.4 12.8s7.7 9.4 9.6 13.6ZM39.6 66.6 28.9 55.7l10.7-10.8 10.9 10.9-10.9 10.8z" fill="#0b3152"/><path d="M57.3 59.8h16.8v32c0 9.1-2.2 16.2-6.7 21.3a25.8 25.8 0 0 1-10.9 7.4 43.7 43.7 0 0 1-15.2 2.4h-7.7a37.5 37.5 0 0 1-16-3.3A28.8 28.8 0 0 1 5 108.9 34.2 34.2 0 0 1 .1 90.7c0-.8.1-1.7.1-2.5.4-8 2.9-16.9 7.4-26.6l15.3 7C19.3 76.4 17.3 83.2 17 89v1.8a15.9 15.9 0 0 0 2.3 9 12.1 12.1 0 0 0 7 5.1 22.3 22.3 0 0 0 3.9.9l3.4.2h7.7c6.4 0 10.8-1.2 13.1-3.6s2.9-5.5 2.9-10.6Z" fill="#0b3152"/><path d="M208.2 174.9a23.1 23.1 0 0 0-3.2-7.3c-4-5.9-10.7-8.8-19.9-8.8a31.6 31.6 0 0 0-14.7 3.8l-3.6 2.1.2-1.8v-31.3h-16.8v51.7l-3.5 5.6h-40.3c-2.4 0-4.1-.6-5-1.7a8.4 8.4 0 0 1-1.2-4.5v-12.5H83.3v15.9c0 2.2-1.2 3.4-3.7 3.4H18.9a2.3 2.3 0 0 1-2.2-1.4 4.3 4.3 0 0 1-.4-1.7c0-.2.1-.5.1-.8a119.4 119.4 0 0 1 3.5-15.3l-16.2-4.6a3.9 3.9 0 0 1-.2.6l-.3 1.2c-.4 1.3-.7 2.6-1 3.9a52.9 52.9 0 0 0-1.9 9.1A35.2 35.2 0 0 0 0 185c0 1.4.1 2.7.2 3.9a20.7 20.7 0 0 0 3.3 9 17.1 17.1 0 0 0 12.1 7.7c2.7.5 8.9.7 18.5.7h29.2c8.8 0 15-.2 18.4-.6a26 26 0 0 0 11.2-3.4c2.8 2.3 7.4 3.4 13.5 3.4h80.2a19.7 19.7 0 0 0 3.4-.4 25.6 25.6 0 0 0 8.2-2.8c7.1-4.1 10.7-11.2 10.7-21.3a31.8 31.8 0 0 0-.7-6.3Zm-15.1 9.1a5.3 5.3 0 0 1-1 2.4c-1.4 1.7-4 2.5-7.9 2.5h-19.8a25.5 25.5 0 0 1 2.9-2.9c1.8-1.6 3.6-3.1 5.5-4.5 5.3-3.9 9.7-5.9 13.2-5.9s7.3 1.9 7.3 5.7a17.6 17.6 0 0 1-.2 2.7ZM51.5 212.8l10.9 10.9-10.8 10.8-10.8-10.9 10.7-10.8z" fill="#187dba"/></svg>
-					<p>شـرکت کارن طب، با بهره‌مندی از متـخصصین مجرب، همواره در تلاش برای بومی‌سازی فناوری‌های پیشرفته و پیشگامی در زمینه فناوری‌های نوین بوده است. تیم دانش‌بنیان تحقیق و توسعه این شرکت توانسته است، با ادغام نوآوری‌های بی‌بـدیل و بومی‌سازی فناوری‌های روز دنیا، به دستاوردهای چـشمگیری در زمینه طراحی و تولید تجهیزات پزشکی دست یابد.</p>
+				<img class="lowRadius" src="<?php echo get_template_directory_uri() ?>/img/Logo Test.png" alt="کارن طب">
+					<p>Karen Teb Company, by using experienced specialists, has always been trying to localize advanced technologies and pioneer in recent medical innovations. The knowledge-based research and development team of this company has been able to achieve remarkable achievements in designing and producing medical equipment.</p>
 				</div>
 				<div class="footerColumn2 footerColumns">
-					<h3>لینک ها</h3>
-					<li><a href="https://karenmed.ir/">صفحه اصلی</a></li>
-					<li><a href="https://karenmed.ir/product/%d8%af%d8%b3%d8%aa%da%af%d8%a7%d9%87-%da%a9%d8%b1%d8%a8%d9%88%da%a9%d8%b3%db%8c-%d8%aa%d8%b1%d8%a7%d9%be%db%8c-%da%a9%d8%a7%d8%b1%d9%86-%d8%b7%d8%a8/">دستگاه کربوکسی</a></li>
-					<li><a href="https://karenmed.ir/product/plasma-pen/">دستگاه پلاسما</a></li>
-					<li><a href="https://karenmed.ir/product/%d9%81%db%8c%d8%b3-%d9%be%d8%af-%da%a9%d8%a7%d8%b1%d9%86-%d8%b7%d8%a8/">فیس پد</a></li>
-					<li><a href="https://karenmed.ir/%d8%af%d8%b1%d8%a8%d8%a7%d8%b1%d9%87-%d9%85%d8%a7/">درباره ما</a></li>
-					<li><a href="https://karenmed.ir/%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7%d9%85%d8%a7/">تماس با ما</a></li>
+					<h3>links</h3>
+					<li><a href="https://karenmed.ir/">Home</a></li>
+					<li><a href="https://karenmed.ir/product/%d8%af%d8%b3%d8%aa%da%af%d8%a7%d9%87-%da%a9%d8%b1%d8%a8%d9%88%da%a9%d8%b3%db%8c-%d8%aa%d8%b1%d8%a7%d9%be%db%8c-%da%a9%d8%a7%d8%b1%d9%86-%d8%b7%d8%a8/">Carboxytherapy Device</a></li>
+					<li><a href="https://karenmed.ir/product/plasma-pen/">Plasma Device</a></li>
+					<li><a href="https://karenmed.ir/product/%d9%81%db%8c%d8%b3-%d9%be%d8%af-%da%a9%d8%a7%d8%b1%d9%86-%d8%b7%d8%a8/">Face Pad</a></li>
+					<li><a href="https://karenmed.ir/%d8%af%d8%b1%d8%a8%d8%a7%d8%b1%d9%87-%d9%85%d8%a7/">About Us</a></li>
+					<li><a href="https://karenmed.ir/%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7%d9%85%d8%a7/">Contact US</a></li>
 				</div>
 				<div class="footerColumn3 footerColumns">
-					<h3 >ارتباط با ما</h3>
-					<a href="https://maps.app.goo.gl/PdjX44XJRwa2vfsN7" target="_blank"><p>آدرس: جردن، خیابان سلطانی (سایه)، پلاک 67، واحد 5</p></a>
+					<h3 >Contact Us</h3>
+					<a href="https://maps.app.goo.gl/PdjX44XJRwa2vfsN7" target="_blank"><p>Address : Tehran, Soltani St, No. 67</p></a>
 					<div class="contactFooter">
-						<li><span>تلفن:</span><a href="tel://09129729702"><span>09129729702</span></a></li>
-						<!-- <li><span>تلفن:</span><a href="tel://02165579712"><span>26201172-021</span></a></li> -->
-						<li><span>تلفن:</span><a href="tel://02145302"><span>45302-021</span></a></li>
-						<li><span>ایمیل:</span><a href="mailto:info@karenmed.ir" target="_blank"><span>info@karenmed.ir</span></a></li>
+						<li><span>Phone:</span><a href="tel://09129729702"><span>09129729702</span></a></li>
+						<!-- <li><span>Phone:</span><a href="tel://02165579712"><span>26201172-021</span></a></li> -->
+						<li><span>Phone:</span><a href="tel://02145302"><span>45302-021</span></a></li>
+						<li><span>Email:</span><a href="mailto:info@karenmed.ir" target="_blank"><span>info@karenmed.ir</span></a></li>
 					</div>
 				</div>
 				<a referrerpolicy="origin" class="eNemadParent" target="_blank" href="https://trustseal.enamad.ir/?id=312800&amp;Code=qHcSeydOHTfqUTSzUene">
@@ -43,7 +43,7 @@
 	</footer><!-- #colophon -->
 	<div class="footerCopyrightBack">
 		<div class="footerCopyright mainView">
-			<p>کلیه حقوق وبسایت برای کارن طب محفوظ است. طراحی و توسعه توسط <a href="https://idechy.ir/" target="_blank">ایده‌چی</a></p>
+			<p>Copyright © 2024 <a href="https://idechy.ir/" target="_blank">idechy</a>®. All rights reserved.</p>
 			<div class="footerCopyrightIcons">
 				<a href="https://www.instagram.com/karenmed_ir?igsh=Z2Y3bG5wN3JreXF0"><svg width="20" height="20" fill="#187dbb" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="M17.34 5.46a1.2 1.2 0 1 0 1.2 1.2 1.2 1.2 0 0 0-1.2-1.2Zm4.6 2.42a7.59 7.59 0 0 0-.46-2.43 4.94 4.94 0 0 0-1.16-1.77 4.7 4.7 0 0 0-1.77-1.15 7.3 7.3 0 0 0-2.43-.47C15.06 2 14.72 2 12 2s-3.06 0-4.12.06a7.3 7.3 0 0 0-2.43.47 4.78 4.78 0 0 0-1.77 1.15 4.7 4.7 0 0 0-1.15 1.77 7.3 7.3 0 0 0-.47 2.43C2 8.94 2 9.28 2 12s0 3.06.06 4.12a7.3 7.3 0 0 0 .47 2.43 4.7 4.7 0 0 0 1.15 1.77 4.78 4.78 0 0 0 1.77 1.15 7.3 7.3 0 0 0 2.43.47C8.94 22 9.28 22 12 22s3.06 0 4.12-.06a7.3 7.3 0 0 0 2.43-.47 4.7 4.7 0 0 0 1.77-1.15 4.85 4.85 0 0 0 1.16-1.77 7.59 7.59 0 0 0 .46-2.43c0-1.06.06-1.4.06-4.12s0-3.06-.06-4.12ZM20.14 16a5.61 5.61 0 0 1-.34 1.86 3.06 3.06 0 0 1-.75 1.15 3.19 3.19 0 0 1-1.15.75 5.61 5.61 0 0 1-1.86.34c-1 .05-1.37.06-4 .06s-3 0-4-.06a5.73 5.73 0 0 1-1.94-.3 3.27 3.27 0 0 1-1.1-.75 3 3 0 0 1-.74-1.15 5.54 5.54 0 0 1-.4-1.9c0-1-.06-1.37-.06-4s0-3 .06-4a5.54 5.54 0 0 1 .35-1.9A3 3 0 0 1 5 5a3.14 3.14 0 0 1 1.1-.8A5.73 5.73 0 0 1 8 3.86c1 0 1.37-.06 4-.06s3 0 4 .06a5.61 5.61 0 0 1 1.86.34 3.06 3.06 0 0 1 1.19.8 3.06 3.06 0 0 1 .75 1.1 5.61 5.61 0 0 1 .34 1.9c.05 1 .06 1.37.06 4s-.01 3-.06 4ZM12 6.87A5.13 5.13 0 1 0 17.14 12 5.12 5.12 0 0 0 12 6.87Zm0 8.46A3.33 3.33 0 1 1 15.33 12 3.33 3.33 0 0 1 12 15.33Z"/></svg></a>
 				<a href="https://t.me/+989129729702"><svg width="20" height="20" viewBox="0 0 48 48" fill="#187dbb" xmlns="http://www.w3.org/2000/svg"><path d="M41.42 7.309s3.885-1.515 3.56 2.164c-.107 1.515-1.078 6.818-1.834 12.553l-2.59 16.99s-.216 2.489-2.159 2.922c-1.942.432-4.856-1.515-5.396-1.948-.432-.325-8.094-5.195-10.792-7.575-.756-.65-1.62-1.948.108-3.463L33.648 18.13c1.295-1.298 2.59-4.328-2.806-.649l-15.11 10.28s-1.727 1.083-4.964.109l-7.016-2.165s-2.59-1.623 1.835-3.246c10.793-5.086 24.068-10.28 35.831-15.15Z"/></svg></a>
--- page-templates/aboutUs-template.php
+++ page-templates/aboutUs-template.php
@@ -10,15 +10,15 @@ get_header();
 <div class="aboutUsPageBack">
     <div class="aboutUsPage mainView">
         <div class="generalHeading highMargined centeredText bottomBorder">
-            <h1>About <div class="changeColorHeader">KAREN TEB</div></h1>
-            <span>کارن طب تجهیز آرا، تولید و عرضه کننده با کیفیت ترین تجهیزات پزشکی</span>
+            <h1>ABOUT <div class="changeColorHeader">KAREN TEB</div></h1>
+            <span>Karen Teb, manufacturer and supplier of the highest quality medical equipment</span>
         </div>
         <div class="aboutUsSectionOne">
             <div class="aboutUsSeOneRight">
                 <div class="generalHeadig highMargined">
-                    <h3>درباره کارن طب</h3>
+                    <h3>Localization</h3>
                 </div>
-                <p>شـرکت کارن طب تجهیز آرا ، با بهره‌مندی از متـخصصین مجرب، همواره در تلاش برای بومی‌سازی فناوری‌های پیشرفته و پیشگامی در زمینه فناوری‌های نوین بوده است. تیم دانش‌بنیان تحقیق و توسعه این شرکت توانسته است، با ادغام نوآوری‌های بی‌بـدیل و بومی‌سازی فناوری‌های روز دنیا، به دستاوردهای چـشمگیری در زمینه طراحی و تولید تجهیزات پزشکی دست یابد. در راستای ارتقاء سلامت جامعه و برطرف کردن نیاز جامعه پزشکی به آخرین فناوری حوزه سلامت، این شرکت بر آن شد تا با بکارگیری نـزدیک به یک دهه تجربه خود در حوزه فنـاوری الکترونیکی و نیز همکاری با متخصصین دانشگاهی و صنعتی این مرز و بوم، به طراحی و تولید تجهیزات مورد نیاز در این حوزه بپردازد.</p>
+                <p>Karen Teb Company, with the benefit of experienced specialists, has always been trying to localize advanced technologies and pioneer in the field of new technologies. The knowledge-based research and development team of this company has been able to achieve remarkable achievements in the design and production of medical equipment by integrating unparalleled innovations and localization of modern technologies. In order to promote community health and meet the medical community’s need for the latest technology in the field of health, the company decided to use nearly a decade of experience in the field of electronic technology and cooperation with academic and industrial experts in this field, to design And produce the equipment needed in this area.</p>
             </div>
             <div class="aboutUsSeOneLeft">
                 <div class="aboutUsIconsSec">
@@ -27,8 +27,8 @@ get_header();
                             <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.984 2.542c.087.169.109.386.152.82.082.82.123 1.23.295 1.456a1 1 0 0 0 .929.384c.28-.037.6-.298 1.238-.82.337-.277.506-.415.687-.473a1 1 0 0 1 .702.035c.175.076.33.23.637.538l.894.894c.308.308.462.462.538.637a1 1 0 0 1 .035.702c-.058.181-.196.35-.472.687-.523.639-.784.958-.822 1.239a1 1 0 0 0 .385.928c.225.172.636.213 1.457.295.433.043.65.065.82.152a1 1 0 0 1 .47.521c.071.177.071.395.071.831v1.264c0 .436 0 .654-.07.83a1 1 0 0 1-.472.522c-.169.087-.386.109-.82.152-.82.082-1.23.123-1.456.295a1 1 0 0 0-.384.929c.038.28.299.6.821 1.238.276.337.414.505.472.687a1 1 0 0 1-.035.702c-.076.175-.23.329-.538.637l-.894.893c-.308.309-.462.463-.637.538a1 1 0 0 1-.702.035c-.181-.058-.35-.196-.687-.472-.639-.522-.958-.783-1.238-.82a1 1 0 0 0-.929.384c-.172.225-.213.635-.295 1.456-.043.434-.065.651-.152.82a1 1 0 0 1-.521.472c-.177.07-.395.07-.831.07h-1.264c-.436 0-.654 0-.83-.07a1 1 0 0 1-.522-.472c-.087-.169-.109-.386-.152-.82-.082-.82-.123-1.23-.295-1.456a1 1 0 0 0-.928-.384c-.281.037-.6.298-1.239.82-.337.277-.506.415-.687.473a1 1 0 0 1-.702-.035c-.175-.076-.33-.23-.637-.538l-.894-.894c-.308-.308-.462-.462-.538-.637a1 1 0 0 1-.035-.702c.058-.181.196-.35.472-.687.523-.639.784-.958.821-1.239a1 1 0 0 0-.384-.928c-.225-.172-.636-.213-1.457-.295-.433-.043-.65-.065-.82-.152a1 1 0 0 1-.47-.521C2 13.286 2 13.068 2 12.632v-1.264c0-.436 0-.654.07-.83a1 1 0 0 1 .472-.522c.169-.087.386-.109.82-.152.82-.082 1.231-.123 1.456-.295a1 1 0 0 0 .385-.928c-.038-.281-.3-.6-.822-1.24-.276-.337-.414-.505-.472-.687a1 1 0 0 1 .035-.702c.076-.174.23-.329.538-.637l.894-.893c.308-.308.462-.463.637-.538a1 1 0 0 1 .702-.035c.181.058.35.196.687.472.639.522.958.783 1.238.821a1 1 0 0 0 .93-.385c.17-.225.212-.635.294-1.456.043-.433.065-.65.152-.82a1 1 0 0 1 .521-.471c.177-.07.395-.07.831-.07h1.264c.436 0 .654 0 .83.07a1 1 0 0 1 .522.472ZM12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" fill="#fff"></path></svg>
                         </div>
                         <div class="taxtPart">
-                            <h5>تضمین کیفیت</h5>
-                            <p>کارن طب با تولید محصولات باکیفیت و مطابق با آخرین استانداردهای روز دنیا، سعی بر جلب اعتماد مشتریان خود دارد.</p>
+                            <h5>Quality Guarantee</h5>
+                            <p>Karen Teb tries to gain the trust of its customers by producing quality products with the latest global standards.</p>
                         </div>
                     </div>
                     <div class="aboutUsIconParent bottomBorder">
@@ -36,8 +36,8 @@ get_header();
                             <svg width="40" height="40" fill="#fff" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M151.003 413.483h249.375a13.125 13.125 0 0 0 0-26.25H151.003a13.125 13.125 0 0 1 0-26.25h249.375a13.125 13.125 0 0 0 0-26.25H151.003a52.5 52.5 0 0 0-52.5 52.5v26.25a52.5 52.5 0 0 0 52.5 52.5h249.375a13.125 13.125 0 0 0 0-26.25H151.003c-17.186-.271-17.307-25.902 0-26.25Zm262.494-118.136V59.134a13.117 13.117 0 0 0-13.117-13.117H164.124v262.466H400.36a13.136 13.136 0 0 0 13.136-13.136Zm-54.686-91.83h-140v-87.5h140ZM137.874 47.737a52.475 52.475 0 0 0-39.377 50.78v230.119a78.466 78.466 0 0 1 39.377-19.003Z"></path></svg>
                         </div>
                         <div class="taxtPart">
-                            <h5>دانش و تخصص</h5>
-                            <p>محصولات کارن طب با بهره‌گیری از تجربیات و نظرات متخصصین پوست، مو و زیبایی، طبق به‌روزترین دانش تخصصی دنیا تولید و به‌روزرسانی می‌شود.</p>
+                            <h5>Knowledge and Expertise</h5>
+                            <p>Karen Teb products are produced and updated with the most modern global knowledge by the experiences and opinions of experts.</p>
                         </div>
                     </div>
                     <div class="aboutUsIconParent bottomBorder">
@@ -45,8 +45,8 @@ get_header();
                             <svg width="40" height="40" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="-20 -15 270 250" xml:space="preserve"><path d="M214.419 32.12A7.502 7.502 0 0 0 209 25.927L116.76.275a7.496 7.496 0 0 0-4.02 0L20.5 25.927a7.5 7.5 0 0 0-5.419 6.193c-.535 3.847-12.74 94.743 18.565 139.961 31.268 45.164 77.395 56.738 79.343 57.209a7.484 7.484 0 0 0 3.522 0c1.949-.471 48.076-12.045 79.343-57.209 31.305-45.217 19.1-136.113 18.565-139.961zm-40.186 53.066-62.917 62.917c-1.464 1.464-3.384 2.197-5.303 2.197s-3.839-.732-5.303-2.197l-38.901-38.901a7.497 7.497 0 0 1 0-10.606l7.724-7.724a7.5 7.5 0 0 1 10.606 0l25.874 25.874 49.89-49.891a7.497 7.497 0 0 1 10.606 0l7.724 7.724a7.5 7.5 0 0 1 0 10.607z"></path></svg>
                         </div>
                         <div class="taxtPart">
-                            <h5>شرایط خرید امن</h5>
-                            <p>امکان دریافت دمو رایگان پیش از خرید و شرایط خرید اقساطی دستگاه‌های کربوکسی تراپی و پلاسما.</p>
+                            <h5>Secure Purchase Conditions</h5>
+                            <p>The possibility of receiving a free demo before purchase and installment purchase conditions of Carboxytherapy and Plasma devices</p>
                         </div>
                     </div>
                     <div class="aboutUsIconParent bottomBorder">
@@ -54,8 +54,8 @@ get_header();
                             <svg width="40" height="40" fill="#fff" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M422.401 217.174c-6.613-67.84-46.72-174.507-170.666-174.507-123.948 0-164.054 106.667-170.668 174.507-23.2 8.805-38.503 31.079-38.4 55.893v29.867c0 32.99 26.745 59.733 59.734 59.733 32.99 0 59.734-26.744 59.734-59.733v-29.867c-.108-24.279-14.848-46.095-37.334-55.253 4.267-39.254 25.174-132.48 126.934-132.48 101.76 0 122.453 93.226 126.72 132.48-22.44 9.178-37.106 31.009-37.12 55.253v29.867a59.947 59.947 0 0 0 33.92 53.76c-8.96 16.853-31.787 39.68-87.894 46.506-11.215-17.03-32.914-23.744-51.788-16.023-18.873 7.72-29.646 27.717-25.71 47.725 3.936 20.009 21.48 34.432 41.872 34.432a42.667 42.667 0 0 0 37.973-23.68c91.52-10.454 120.747-57.6 129.92-85.334 24.817-8.039 41.508-31.301 41.173-57.386v-29.867c.103-24.814-15.2-47.088-38.4-55.893Zm-302.933 85.76c0 9.425-7.641 17.066-17.067 17.066-9.425 0-17.066-7.64-17.066-17.066v-29.867a17.067 17.067 0 1 1 34.133 0v29.867Zm264.533-29.867c0-9.426 7.641-17.067 17.067-17.067s17.067 7.641 17.067 17.067v29.867c0 9.425-7.641 17.066-17.067 17.066s-17.067-7.64-17.067-17.066v-29.867Z" fill-rule="evenodd"></path></svg>
                         </div>
                         <div class="taxtPart">
-                            <h5>خدمات پس از فروش</h5>
-                            <p>تمام دستگاه‌های تولید شرکت کارن طب، شامل گارانتی، خدمات پس از فروش، آموزش و پشتیبانی 24 ساعته است.</p>
+                            <h5>Customer Service</h5>
+                            <p>All devices manufactured by Karen Teb Company include warranty, after-sales service, training and 24-hour support.</p>
                         </div>
                     </div>
                 </div>
@@ -64,8 +64,8 @@ get_header();
         <div class="aboutUsSectionTwo lightBoxBack highRadius mediumPadding mediumMargined">
             <div class="sectionTwoInner">
                 <div class="textSection">
-                    <h4>توسعه اقتصادی</h4>
-                    <p class="noMargin justified">کارگروه دانش بنیان تحقیق و توسعه کارن طب تجهیز آرا ، متشکل از متخصصین مجرب در شاخه‌های علوم مختلف است که با بهره‌گیری از تجربیات و نظرات پزشکان، بخصوص متخصصین پوست، مو و زیبایی، همواره به دنبال ارائه روش‌های جدید و بهبود روش‌های قدیمی است. امیدواریم که تولید محصولات با کیفیت مطابق با آخرین استانداردها و نیز صـادرات محصولات دانش بـنیان در منطقه، گامی کوچک در راستـای توسعه اقتصادی کشور و سربلندی ایران عزیزمان باشد.</p>
+                    <h4>Economic Development</h4>
+                    <p class="noMargin justified">Karen Teb Research and Development Knowledge-Based Working Group consists of experienced specialists in various fields of science who, using the experiences and opinions of physicians, especially dermatologists, hair and beauty specialists, always seeks to provide new methods and improve old methods. We hope that the production of quality products in accordance with the latest standards and the export of knowledge-based products in the region is a small step towards the economic development of the country and the pride of our dear Iran.</p>
                 </div>
                 <div class="imgSection">
                     <img class="lowRadius" src="<?php echo get_template_directory_uri() ?>/img/karenteb-our-team.jpg" alt="تیم کارن طب">
@@ -73,8 +73,8 @@ get_header();
             </div>
         </div>
         <div class="aboutUsSectionThree mediumPadding">
-            <h4>خدمات</h4>
-            <p class="noMargin justified">کارگروه دانش بنیان تحقیق و توسعه کارن طب تجهیز آرا ، متشکل از متخصصین مجرب در شاخه‌های علوم مختلف است که با بهره‌گیری از تجربیات و نظراتکارشناسان حرفه‌ای شرکت کارن طب تجهیز آرا، همواره متعهد به ارائه بهترین و به‌روزترین راهکارها به پزشکان و متخصصین هستند. همچنین شما می‌توانید در هر زمان با تماس با هر یک از کارشناسان فروش این مجموعه، به‌روزترین اطلاعات و راهکارهای حوزه تخصصی خود را جویا و از مشاوره تخصصی آن‌ها بهره‌مند شوید. آنچه که با خرید هر یک از محصولات کارن طب تجهیز آرا دریافت می‌کنید، شامل پکیج تخصصی آموزش جامع پزشکان، ضمانت یکساله تعویض بی‌قید و شرط، خدمات پس از فروش در سراسر کشور و روش‌های متنوع فروش، مطابق با نیاز و درخواست شماست.</p>
+            <h4>Services</h4>
+            <p class="noMargin justified">Karen Teb Research and Development Knowledge-Based Working Group consists of experienced specialists in various fields of science who, using the experiences and opinions of Karen Teb’s professional experts, are always committed to providing the best and most up-to-date solutions to physicians and specialists. You can also contact any of the sales experts in this collection at any time to seek the latest information and solutions in your field and benefit from their expert advice. What you get with each purchase of Karen Teb products includes a specialized package of comprehensive training for doctors, a one-year unconditional replacement guarantee, after-sales service throughout the country and various sales methods, according to your needs and requests.</p>
         </div>
     </div>
 </div>
